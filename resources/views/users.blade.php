33c1f80aaf9c606c9490a2360743f2ae259f2588
unknown <binurim95@gmail.com>
2018-08-30 17:04:59 +0530

    add new files

---
+++ resources/views/users.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<p>Users register Page</p>
+	
+	<form class="" action="/register" method="post">
+		
+		 {{ csrf_field() }}
+		<label for="">Name</label>
+		<input type="text" name="name"><br>
+		<label for="">Email</label>
+		<input type="email" name="email"><br>
+		<label for="">Password</label>
+		<input type="password" name="password">
+
+		<input type="submit" name="register" value="register now">
+		
+
+
+	</form>
+
+</body>
+</html>
